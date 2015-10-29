c11f3f0ff0056040728b138ff9a5450a667b05db
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-29 10:59:25 -0500

    showing data & photos

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
-L.amigo.auth.setToken("R:AJYcFuYfi2KJOCaYmLN3uNKvkzceCturgM4EWz");
+TOKEN = "R:Z6iIcrcXhuN6mxbvolFrBQFBP4acr7bRyqbTPf";
+L.amigo.auth.setToken(TOKEN);
 
 var map = new L.amigo.map(
     'map',
@@ -16,6 +17,23 @@ map.addDatasetLayer({
     url: 'https://www.amigocloud.com/api/v1/users/475/projects/1635/datasets/28987',
     popup: {
         popupTitle: 'description',
-        displayFields: ['date']
+        className: 'amigo-popup',
+        displayFields: ['description', 'date'],
+        additionalCallback: function (e, map) {
+            if (e.data) {
+                L.amigo.utils.get(
+                    'https://www.amigocloud.com/api/v1/related_tables/1122/entries',
+                    {source_amigo_id: e.data.amigo_id})
+                    .then(function (r) {
+                        var html = '', url;
+                        for (var i in r.data) {
+                            url = 'https://www.amigocloud.com/api/v1/related_tables/1122/files/' +
+                                e.data.amigo_id + '/' + r.data[i].filename + '?token=' + TOKEN;
+                            html += '<img class="img-responsive img-thumbnail" src="' + url + '"/>'
+                        }
+                        $('#photos').html(html);
+                    });
+            }
+        }
     }
 });
