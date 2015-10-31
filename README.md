36ec83bfae98f4ed0b4cd2e92119001727af5edc
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-31 01:28:24 -0500

    moving around in the map
    angular included

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# node-js-sample
+# cs-trip
 
-A barebones Node.js app using [Express 4](http://expressjs.com/).
+Geo slides using amigocloud.js
+
+## Author
+
+- [Aldo Culquicondor](https://github.com/alculquicondor)
 
 ## Running Locally
 
 Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
 
 ```sh
-git clone git@github.com:heroku/node-js-sample.git # or clone your own fork
-cd node-js-sample
+git clone git@github.com:alculquicondor/cs-trip.git # or clone your own fork
+cd cs-trip
 npm install
 npm start
 ```
@@ -22,17 +26,3 @@ heroku create
 git push heroku master
 heroku open
 ```
-
-Alternatively, you can deploy your own copy of the app using the web-based flow:
-
-[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## Documentation
-
-For more information about using Node.js on Heroku, see these Dev Center articles:
-
-- [10 Habits of a Happy Node Hacker](https://blog.heroku.com/archives/2014/3/11/node-habits)
-- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
-- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
-- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
-- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    L.amigo.auth.setToken("R:Z6iIcrcXhuN6mxbvolFrBQFBP4acr7bRyqbTPf");
+var csTripApp = angular.module('csTripApp', []);
 
-    var map = new L.amigo.map(
+csTripApp.controller('CsTripCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.map = new L.amigo.map(
         'map',
         {
             center: [0, 0],
@@ -10,35 +10,127 @@ $(document).ready(function() {
             amigoLogo: 'right'
         }
     );
+    $scope.route = [];
+    $scope.positionMap = {};
+    $scope.images = [];
+    $scope.datasetData = {};
+    $scope.position = -1;
 
-    map.addBaseLayer(L.amigo.AmigoGray);
+    L.amigo.auth.setToken("R:Z6iIcrcXhuN6mxbvolFrBQFBP4acr7bRyqbTPf");
 
-    map.addDatasetLayer({
-        url: 'https://www.amigocloud.com/api/v1/users/475/projects/1635/datasets/28987',
-        popup: {
-            popupTitle: 'description',
-            className: 'amigo-popup',
-            displayFields: ['description', 'date'],
-            additionalCallback: function (e, map) {
-                if (e.data) {
-                    L.amigo.utils.get(
-                        'https://www.amigocloud.com/api/v1/related_tables/1122/entries',
-                        {source_amigo_id: e.data.amigo_id})
-                        .then(function (r) {
-                            var html = '', url;
-                            for (var i in r.data) {
-                                url = 'https://www.amigocloud.com/api/v1/related_tables/1122/files/' +
-                                    e.data.amigo_id + '/' + r.data[i].filename + L.amigo.auth.getTokenParam();
-                                html += '<img class="img-responsive img-thumbnail" src="' + url + '"/>'
-                            }
-                            $('#photos').html(html);
-                        });
-                }
-            }
+    L.amigo.utils.get('/users/475/projects/1635/sql', {
+        query: 'SELECT ST_X(location) AS lng, ST_Y(location) AS lat, amigo_id FROM dataset_28987 ORDER BY "order"'
+    }).then(function (r) {
+        $scope.route = r.data;
+        for (var i in $scope.route) {
+            $scope.positionMap[$scope.route[i].amigo_id] = i;
         }
     });
 
-    setTimeout(function () {
+    $scope.map.addBaseLayer(L.amigo.AmigoGray);
+
+    $scope.previousSlide = function () {
+        if ($scope.position >= 0) {
+            --$scope.position;
+            if ($scope.position >= 0) {
+                $scope.goToPosition();
+            }
+        }
+    };
+
+    $scope.nextSlide = function () {
+        if ($scope.position < $scope.route.length - 1) {
+            ++$scope.position;
+            $scope.goToPosition();
+        }
+    };
+
+    $scope.goToPosition = function () {
+        var step = $scope.route[$scope.position];
+        $scope.map.setView(step, 8, {
+            pan: {duration: 1},
+            animate: true
+        });
+        $scope.showPopup();
+        $scope.loadPhotos();
+    };
+
+    $scope.loadPhotos = function (amigoId) {
+        if (amigoId === undefined) {
+            amigoId = $scope.route[$scope.position].amigo_id;
+        }
+
+        L.amigo.utils.get(
+            '/related_tables/1122/entries', {source_amigo_id: amigoId})
+            .then(function (r) {
+                $scope.$apply(function () {
+                    var url;
+                    $scope.images = [];
+                    for (var i in r.data) {
+                        url = L.amigo.utils.parseUrl('/related_tables/1122/files/' +
+                            amigoId + '/' + r.data[i].filename +
+                            L.amigo.auth.getTokenParam());
+                        $scope.images.push(url);
+                    }
+                });
+            });
+    };
+
+    $scope.showPopup = function (amigoId) {
+        if (amigoId === undefined) {
+            amigoId = $scope.route[$scope.position].amigo_id;
+        }
+
+        var step = $scope.route[$scope.positionMap[amigoId]],
+            sql = "SELECT title, description FROM " + $scope.datasetData.table_name +
+                " WHERE amigo_id='" + amigoId + "'";
+        L.amigo.utils.get('/users/475/projects/1635/sql', {query: sql})
+            .then(function (r) {
+                if (r.data.length > 0) {
+                    var data = r.data[0],
+                        html = '<h3>' + data.title + '</h3><p>' + data.description + '</p>';
+                    L.popup().setLatLng(step).setContent(html).openOn($scope.map);
+                }
+            })
+    };
+
+    $scope.$watch('map.datasetLayers["CS Trip"].options.datasetData',
+        function (datasetData, oldValue) {
+            if (datasetData !== oldValue) {
+                $scope.datasetData = datasetData;
+                if (!$scope.map.utfGrids) {
+                    $scope.map.utfGrids = {};
+                }
+
+                $scope.map.utfGrids[datasetData.name] = L.utfGrid(
+                    datasetData.tiles + '/{z}/{x}/{y}.json' + L.amigo.auth.getTokenParam(),
+                    {
+                        useJsonP: false,
+                        datasetData: datasetData
+                    }
+                ).on('click', function (e) {
+                        if (e.data) {
+                            var amigoId = e.data.amigo_id, idx;
+
+                            $scope.showPopup(amigoId);
+                            $scope.loadPhotos(amigoId);
+                            $scope.$apply(function () {
+                                $scope.position = $scope.positionMap[amigoId];
+                            });
+                        }
+                    }
+                );
+
+                $scope.map.addLayer($scope.map.utfGrids[datasetData.name]);
+            }
+        }
+    );
+
+    $scope.map.addDatasetLayer({
+        url: '/users/475/projects/1635/datasets/28987'
+    });
+
+    $timeout(function () {
         $('.leaflet-control-layers-selector').click();
     }, 2000);
-});
+}]);
